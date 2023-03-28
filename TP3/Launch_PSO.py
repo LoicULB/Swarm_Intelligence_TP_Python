e26ad928bde6fbbcf73380098acc36fad743eabd
LoicULB <loic.quivron@ulb.be>
2023-03-28 09:05:33 +0200

    Begin exercice 4

--- TP3/Launch_PSO.py
+++ TP3/Launch_PSO.py
@@ -37,8 +37,32 @@ def exercice_3():
                 print(st)
                 subprocess.run(st, check=True, shell=True)
 
+def exercice_4():
+    nb_particule = 20
+    topology = "wheel"
+    inertia_range = [0.1, 0.4, 0.8, 1]
+    phi1_range = [0.2, 0.7, 1, 2]
+    phi2_range = [0.2, 0.7, 1, 2]
+    for inertia in inertia_range:
+        for phi1 in phi1_range:
+            for phi2 in phi2_range:
+                for run in range(NB_RUN):
+                    st = f"{EXE_PATH} --rosenbrock" \
+                       f" --save_file results/exo_4/intertia_{inertia}_phi1_{phi1}_phi2_{phi2}_run_{run}.csv" \
+                       f" --seed {1234 + run}"\
+                       f" --iterations {NB_ITERATIONS}"\
+                       f" --{topology} "\
+                       f" --particles {nb_particule}"\
+                        f" --inertia {inertia}" \
+                       f" --phi1 {phi1}"\
+                       f" --phi2 {phi2}"\
+                       f" --n 10"
+                    print(st)
+                    subprocess.run(st, check=True, shell=True)
+
 
 if __name__ == '__main__':
 
     #exercice_2()
-    exercice_3()
+    #exercice_3()
+    exercice_4()
